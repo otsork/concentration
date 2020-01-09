a3c8536e8d4075d2443fc095a690f4231cb2b2fa
Timo Mämmi <timo.mammi@gmail.com>
2020-01-09 15:37:39 +0200

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 export default function Menu() {
     return (
         <div>
-        <button>Aloita</button>
-        <button>Lopeta</button>
+            <button>Aloita</button>
+            <button>Lopeta</button>
         </div>
     )
 }
