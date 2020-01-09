7a72ef96740983960c10a795b90059b8116cbc18
Timo Mämmi <timo.mammi@gmail.com>
2020-01-09 15:07:16 +0200

    Menu component added

--- src/containers/ManagerContainer.jsx
+++ src/containers/ManagerContainer.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import Background from '../components/Background'
-import Clock from '../components/Clock'
+//import Clock from '../components/Clock'
+import Menu from '../components/Menu'
+
 
 export default function ManagerContainer() {
   const [count, setCount] = useState(0)
 
   const angle = count * 6
   return (
-    <div>
+    /*<div>
       <Background text='text, just to show how props work in functional components' updateTimer={() => setCount(count + 1)}>
-        <Clock angle={angle} />
+      <Clock angle={angle} />
+      </Background>
+    </div>*/
+    <div>
+      <Background>
+      <Menu >
+      </Menu>
       </Background>
     </div>
   )
